1541eb7bfd4f1373e44172b7a0e705225b1117e3
gad <nday.gady@gmail.com>
2025-09-13 20:02:33 -0700

    dev infrastructure pipeline

---
+++ remote_backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket       = "remote-gn-terrraform"
+    key          = "demo-docker/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = false
+  }
+}
