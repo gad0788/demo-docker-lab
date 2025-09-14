b6c928873a4cdd6905a14dc02ab49f3e0f58f3fc
gad <nday.gady@gmail.com>
2025-09-13 20:50:16 -0700

    dev infrastructure pipeline

--- remote_backend.tf
+++ remote_backend.tf
@@ -6,15 +6,5 @@ terraform {
     encrypt      = true
    # use_lockfile = false
   }
-    required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source = "hashicorp/aws"
-    }
-  }
-}
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+  
 }
