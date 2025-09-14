03ce268f8b0cfb8f29cbe2f12e1388aee315e8fd
gad <nday.gady@gmail.com>
2025-09-13 20:43:13 -0700

    dev infrastructure pipeline

--- remote_backend.tf
+++ remote_backend.tf
@@ -3,6 +3,18 @@ terraform {
     bucket       = "remote-gn-terrraform"
     key          = "demo-docker/terraform.tfstate"
     region       = "us-east-1"
+    encrypt      = true
    # use_lockfile = false
   }
+    required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
